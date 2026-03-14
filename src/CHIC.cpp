b827b4fd5c5ed04320ef065f7972b33e6d1df036
pablo <pablo.fercern.ch>
2026-03-14 03:19:16 +0100

    Fix wrong sign artifact in eigenvalues from Vieta eq.

--- src/CHIC.cpp
+++ src/CHIC.cpp
@@ -158,9 +158,12 @@ void CHIC::_compute_hamiltonians() {
     if (TrHs2 < EPSILON) {
         throw std::invalid_argument(
             "Eigenvalues are (almost) degenerate, please check your input parameters.");
-    } else {        
+    } else {
         lambdas[2] = std::sqrt(OptConstants::TWO_THIRDS * TrHs2) *
             std::cos(std::acos(DetHs * std::sqrt(54.0 / trHs2_cubed)) * OptConstants::INV_3);
+        if (DetHs < 0.0) {
+            lambdas[2] = -lambdas[2];
+        }
         lambdas[1] = -(0.5 * lambdas[2] -
             std::sqrt(0.25 * lambdas[2] * lambdas[2] - DetHs / lambdas[2]));
         lambdas[0] = -(lambdas[2] + lambdas[1]);
